3c713b1d6d3aa055d4a70c2e88cd8a376972df36
Shriansh16 <shrianshsingh20@gmail.com>
2024-07-09 16:21:18 +0530

    updated

--- updated_chatbot/app.py
+++ updated_chatbot/app.py
@@ -29,3 +29,28 @@ human_msg_template=HumanMessagePromptTemplate.from_template(template="{input}")
 
 prompt_template=ChatPromptTemplate.from_messages([system_msg_template,MessagesPlaceholder(variable_name="history"),human_msg_template])
 conversation=ConversationChain(memory=st.session_state.buffer_memory,prompt=prompt_template,llm=llm,verbose=True)
+
+#container for chat history
+response_container=st.container()
+#container for test box
+textcontainer=st.container()
+
+with textcontainer:
+    query=st.text_input("Query: ",key="input")
+    if query:
+        with st.spinner("typing..."):
+            conversation_string=get_conversation_string()
+            refined_query=query_refiner(conversation_string,query)
+            st.subheader("Refined query")
+            st.write(refined_query)
+            context=find_match(refined_query)
+            response=conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
+        st.session_state.requests.append(query)
+        st.session_state.responses.append(response)
+
+with response_container:
+    if st.session_state['responses']:
+        for i in range(len(st.session_state['responses'])):
+            message(st.session_state['responses'][i],key=str(i))
+            if i<len(st.session_state['requests']):
+                message(st.session_state['requests'][i], is_user=True,key=str(i)+'_user')
